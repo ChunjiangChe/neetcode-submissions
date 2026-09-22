3577a48184245988ba93483268808f1771e78618
Chunjiang Che <skt.faker.ccj@gmail.com>
2026-09-22 12:43:04 -0400

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for(int i = 0;i < n;i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int target = -nums[i];
+            int left = i+1;
+            int right = n-1;
+            while(left < right) {
+                if(nums[left] + nums[right] < target) {
+                    left ++;   
+                } else if (nums[left] + nums[right] > target) {
+                    right --;
+                } else {
+                    vector<int> item{nums[i], nums[left], nums[right]};
+                    res.push_back(item);
+                    left ++;
+                    right --;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                }
+            }
+        }
+        return res;
+    }
+};
